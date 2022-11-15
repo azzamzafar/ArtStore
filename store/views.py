2fa3d5f951c9224f0f8edb6e96d8ff0d8672acec
Azzam Zafar <33324318+azzamzafar@users.noreply.github.com>
2022-11-15 12:29:17 +0530

    action in request.post not forms

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.db.models import Sum
 from django.views.generic import DetailView, FormView, ListView
 from django.views.generic.list import MultipleObjectMixin
@@ -21,53 +22,59 @@ class ProductListView(MultipleObjectMixin, FormView):
             return redirect(self.request.path_info)
         elif self.request.POST.get('action')=='order':
             return reverse('invoice')
+
+    def form_valid(self,form):
+                   
+        if self.request.POST.get("action") == "item":
+            user = Customer.objects.filter(email = self.request.user)[0]
+            cart_obj = Cart.objects.get_or_create(customer=user)[0]
+            product_id = form.cleaned_data.get("product")
+            prod_obj = Product.objects.filter(id=product_id)[0]
+            price = prod_obj.price
+            item_obj = Item.objects.create(
+                product=prod_obj,
+                price=price,
+                Qty=form.cleaned_data.get("Qty"),
+                cart=cart_obj,
+            )
+            item_obj.save()
+            
+        elif self.request.POST.get("action")== "order":
+            user = Customer.objects.filter(email = self.request.user)[0]
+            if self.request.user.is_anonymous:
+                redirect("login")
+            elif not user.address1:
+                redirect("profile")
+            invoice_obj = Invoice.objects.get_or_create(customer=user)[0]
+            product_id = form.cleaned_data.get("product")
+            prod_obj = Product.objects.filter(id=product_id)[0]
+            price = prod_obj.price
+            order_obj = Order.objects.create(
+                product=prod_obj,
+                price=price,
+                Qty=form.cleaned_data.get("Qty"),
+                invoice=invoice_obj,
+            )
+            order_obj.save()
+        return self.get_success_url()
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
     
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["form"] = ItemsForm()
-        # context['order_formset']  = OrdersFormSet(prefix='order_form')
         return context
 
     def post(self, request, *args, **kwargs):
         form = ItemsForm(request.POST)
-        print(request.user)
-        # order_form = get_form(request,OrdersFormSet,"order_form")
-        
         if form.is_valid():
-            
-            if form.cleaned_data.get("action") == "item":
-                user = Customer.objects.filter(email = request.user)[0]
-                cart_obj = Cart.objects.get_or_create(customer=user)
-                product_id = form.cleaned_data.get("product")
-                prod_obj = Product.objects.filter(id=product_id)[0]
-                price = Product.objects.filter(id=product_id)[0].price
-                item_obj = Item.objects.create(
-                    product=prod_obj,
-                    price=price,
-                    Qty=form.cleaned_data.get("Qty"),
-                    cart=cart_obj,
-                )
-                item_obj.save()
-                return redirect('home')
-            elif form.cleaned_data.get("action")== "order":
-                user = Customer.objects.filter(email = request.user)[0]
-                if request.user.is_anonymous:
-                    redirect("login")
-                elif not user.address1:
-                    redirect("profile")
-                invoice_obj = Invoice.objects.get_or_create(customer=user)
-                product_id = form.cleaned_data.get("product")
-                prod_obj = Product.objects.filter(id=product_id)[0]
-                price = Product.objects.filter(id=product_id)[0].price
-                order_obj = Order.objects.create(
-                    product=prod_obj,
-                    price=price,
-                    Qty=form.cleaned_data.get("Qty"),
-                    invoice=invoice_obj,
-                )
-                order_obj.save()
-
-                redirect("/invoice/")
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+        
+        
 
 
 class ProductDetailView(DetailView):
@@ -77,17 +84,14 @@ class ProductDetailView(DetailView):
 
 def cartview(request):
     my_cart = Cart.objects.filter(customer=request.user)[0]
-    my_items = Item.objects.filter(cart=my_cart)
-    cart_total = (
-        Item.objects.filter(cart=my_cart)
-        .aggregate(Sum("order_amount"))
-        .get("order_amount__sum")
-    )
-    cart_Qty = Item.objects.filter(cart=my_cart).aggregate(Sum("Qty")).get("Qty__sum")
+    # my_items = Item.objects.filter(cart=my_cart)
+    my_cart.cart_Qty = Item.objects.filter(cart=my_cart).aggregate(Sum("Qty")).get("Qty__sum")
+    my_cart.cart_total = Item.objects.filter(cart=my_cart).aggregate(Sum("order_amount")).get("order_amount__sum")
+    my_items = my_cart.user_items.all()
     return render(
         request,
         "store/cart.html",
-        {"total": cart_total, "Qty": cart_Qty, "my_items": my_items},
+        {"total": my_cart.cart_total, "Qty": my_cart.cart_Qty, "my_items": my_items},
     )
 
 
